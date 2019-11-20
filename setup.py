3ff872701378cb5598c327e148d3ad1af02b6b2f
Daniel Haehn <daniel.haehn@umb.edu>
2019-11-19 21:26:06 -0500

    Trying setup.py to get TrakoDracoPy.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="trako", # Replace with your own username
-    version="0.2",
+    version="0.2.dev9",
     author="Daniel Haehn",
     author_email="haehn@mpsych.org",
     description="The TRAKO Project: Compression of DTI Streamlines.",
@@ -19,4 +19,27 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'cycler>=0.10.0',
+        'dataclasses>=0.6',
+        'dataclasses-json>=0.3.6',
+        'kiwisolver>=1.1.0',
+        'marshmallow>=3.2.2',
+        'marshmallow-enum>=1.5.1',
+        'matplotlib>=3.1.1',
+        'mypy-extensions>=0.4.3',
+        'numpy>=1.17.4',
+        'packaging>=19.2',
+        'prettytable>=0.7.2',
+        'pygltflib>=1.11.10',
+        'pyparsing>=2.4.5',
+        'python-dateutil>=2.8.1',
+        'scikit-build>=0.10.0',
+        'six>=1.13.0',
+        'stringcase>=1.2.0',
+        'TrakoDracoPy>=0.0.13.dev9',
+        'typing-extensions>=3.7.4.1',
+        'typing-inspect>=0.5.0',
+        'vtk>=8.1.2'
+    ]
 )
