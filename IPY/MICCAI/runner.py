93c9cb90cb1738bf07ba533cf260f62564e43ee1
Daniel Haehn <daniel.haehn@umb.edu>
2020-03-16 14:55:32 -0400

    Autogenerate plots.

--- IPY/MICCAI/runner.py
+++ IPY/MICCAI/runner.py
@@ -1,6 +1,8 @@
 import os, time
 import numpy as np
 import nibabel as nib
+from dipy.io.dpy import Dpy
+from dipy.tracking.streamline import compress_streamlines
 import pickle
 import trako as TKO
 import vtk
@@ -77,16 +79,24 @@ class Runner:
 
     error = []
 
-    for i,p in enumerate(points1):
+    # we can only calculate results if fiber lengths matches
+    # not good for DPY!!
+    if len(points1) == len(points2):
+      for i,p in enumerate(points1):
 
-      p1 = points1[i]
-      p2 = points2[i]
+        p1 = points1[i]
+        p2 = points2[i]
 
-      dist = np.linalg.norm(p1-p2)
+        dist = np.linalg.norm(p1-p2)
 
-      error.append(dist)
+        error.append(dist)
 
-    error = (np.min(error), np.max(error), np.mean(error), np.std(error))
+      error = (np.min(error), np.max(error), np.mean(error), np.std(error))
+
+    else:
+
+      error = (0, 0, 0, 0)
+
     end_error = [np.linalg.norm(points1[0]-points2[0]), np.linalg.norm(points1[-1]-points2[-1])]
     end_error = (np.min(end_error), np.max(end_error), np.mean(end_error), np.std(end_error))
 
@@ -104,6 +114,102 @@ class Runner:
     c_factor = float(originalsize) / float(compressedsize)
 
     return (originalsize, compressedsize, c_ratio, c_factor)
+
+
+  @staticmethod
+  def dpy(workingdir, input, compressed=None, restored=None, tol_error=0.01, force=False, coords_only=False, verbose=False):
+
+    if not input.endswith('tck') and not input.endswith('trk'):
+      # we need to convert
+      print('Invalid format')
+      return None
+
+    if not compressed:
+      compressed = input + '_compressed'+str(tol_error)+'.dpy'
+
+    if not restored:
+      restored = input + '_restored'+str(tol_error)+'.tck'
+
+    original = os.path.join(workingdir, input)
+
+    #
+    # compression
+    #
+    c_time = -1
+    if not os.path.exists(os.path.join(workingdir, compressed)) or force:
+      # compress again!
+      t0 = time.time()
+
+      loaded_original = nib.streamlines.load(original, lazy_load=False)
+      original_streamlines = loaded_original.streamlines
+      
+      # parameters from Presseau15 are 0.01 and inf
+      c_streamlines = compress_streamlines(original_streamlines, 
+                                           tol_error=tol_error, 
+                                           max_segment_length=np.inf)
+
+      # write dpy file
+      # set compression to highest but it does have any effect
+      dpw = Dpy(os.path.join(workingdir, compressed), mode='w', compression=9)
+      for c_s in c_streamlines:
+          dpw.write_track(c_s)
+      dpw.close()
+
+      c_time = time.time()-t0
+
+      if verbose:
+        print('compression done.')
+
+    #
+    # restoring
+    #
+    d_time = -1
+    if not os.path.exists(os.path.join(workingdir, restored)) or force:
+      # restore again!
+      t0 = time.time()
+      restored_data = Dpy(os.path.join(workingdir, compressed), mode='r')
+      restored_streamlines = restored_data.read_tracks()
+      restored_data.close()
+      d_time = time.time()-t0
+
+      with open(os.path.join(workingdir, restored), 'w') as f:
+        f.write('restoredok.')
+
+      if verbose:
+        print('restoring done.')
+
+    #
+    # calculate errors
+    #
+    stats = compressed + '_stats'+str(tol_error)+'.p'
+    if not os.path.exists(os.path.join(workingdir, stats)) or force:
+
+      statsdata = Runner.error_per_streamlines(original_streamlines, restored_streamlines)
+      sizestatsdata = Runner.sizestats(os.path.join(workingdir, original), os.path.join(workingdir, compressed))
+
+      statsdata = [c_time, d_time, sizestatsdata, statsdata[0], statsdata[1]]
+
+      with open(os.path.join(workingdir, stats), 'wb') as f:
+        pickle.dump(statsdata, f)
+
+    else:
+      with open(os.path.join(workingdir, stats), 'rb') as f:
+        statsdata = pickle.load(f)
+
+
+    [c_time, d_time, sizestatsdata, (min_e, max_e, mean_e, std_e), (end_min_e, end_max_e, end_mean_e, end_std_e)] = \
+      statsdata
+
+    if verbose:
+      print('Times', c_time, d_time)
+      print('Size Stats', sizestatsdata)
+      print('Error', min_e, max_e, mean_e, std_e)
+      print('Endpoint Error', end_min_e, end_max_e, end_mean_e, end_std_e)
+
+    return statsdata
+
+
+
 
 
   @staticmethod
--- IPY/MICCAI/sprinter.py
+++ IPY/MICCAI/sprinter.py
@@ -1,6 +1,132 @@
+import numpy as np
 import runner
 
 class Sprinter:
+
+  @staticmethod
+  def bitsplot(plt, tkoruns={}, tko_bits = [6,7,8,9,10,11,12,13,14], qfibruns=None, dpyruns=None, xlim=None, ylim=(0,1), filename=None):
+    '''
+    '''
+    plt.figure(num=None, figsize=(8, 6), dpi=240, facecolor='w', edgecolor='k')
+
+
+    longest_run = 0
+    longest = 0
+    for r in tkoruns.keys():
+        
+    #     if r != 'default':
+    #         continue
+        
+        tko_sizes = tkoruns[r][0]
+        tko_errors = tkoruns[r][1]
+        tko_stds = tkoruns[r][2]
+
+        if 0 in tko_sizes:
+            tko_sizes.remove(0)
+        if 0 in tko_errors:
+            tko_errors.remove(0)
+        if 0 in tko_stds:
+            tko_stds.remove(0)
+
+        if len(tko_sizes) > longest:
+          # print('lo',r)
+          longest = len(tko_sizes)
+          longest_run = tko_sizes
+                
+        
+        tko_sizes_ = np.array(tko_sizes) / 1000000
+    #     plt.scatter(tko_sizes_, tko_errors, s = np.array(tko_stds)*1000, alpha=1, label='TRAKO ('+r+')')
+        size = 10
+        if r=='default':
+            size=70
+
+        run_name = r
+        if run_name == 'qbponly{bits}':
+          run_name = 'XYZ only'
+        elif run_name == 'qbi{bits}':
+          run_name = 'XYZ + Ind.'
+        elif run_name == 'qbi_CL0_{bits}':
+          run_name = 'XYZ + Ind. Level 0'
+        plt.scatter(tko_sizes_, tko_errors, s = size, alpha=1, label='TRAKO ('+run_name+')')
+        plt.errorbar(tko_sizes_, tko_errors, tko_stds, fmt='|', alpha=.5)
+
+    for x,s in enumerate(longest_run):
+
+      y = tko_errors[x]
+      y += y*0.05
+      txt = tko_bits[x]
+      x = tko_sizes_[x]
+      x += x*0.01
+
+      if ylim:
+        if y<ylim[0]:
+          continue
+        if y>= ylim[1]:
+          continue
+      if xlim:
+        if x<=xlim[0]:
+          continue
+        if x>=xlim[1]:
+          continue
+
+
+      
+      plt.text(x, y, txt)
+
+
+    if qfibruns:
+      qfib_sizes = qfibruns[0]
+      qfib_errors = qfibruns[1]
+      qfib_stds = qfibruns[2]
+      qfib_sizes_ = np.array(qfib_sizes) / 1000000
+      plt.scatter(qfib_sizes_, qfib_errors, s = 10, color='black', alpha=1, label='QFib')
+      plt.errorbar(qfib_sizes_, qfib_errors, qfib_stds, fmt='|', color='black', alpha=.5)
+      plt.ticklabel_format(style='plain', axis='both', scilimits=(0, 0))
+
+    import matplotlib.ticker as mticker    
+    plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%d MB'))
+
+    if ylim:
+      plt.ylim(ylim[0], ylim[1])
+
+    if xlim:
+      plt.xlim(xlim[0], xlim[1])
+
+    # plt.axvline(x=input_size/1000000, color='red', linestyle='--')
+
+    plt.xlabel('Data Size [MB]')
+    plt.ylabel('Mean Absolute Error [MAE]')
+    plt.legend()
+
+    if filename:
+      a = plt.savefig(filename)
+
+    plt.show()
+
+
+
+  @staticmethod
+  def run_dpy(dpy_files):
+    dpy_sizes = [0]
+    dpy_errors = [0]
+    dpy_stds = [0]
+    for f in dpy_files:
+
+        rundata = runner.Runner.dpy(f[0], f[1], force=False)
+        c_time = rundata[0]
+        d_time = rundata[1]
+        sizestats = rundata[2]
+        compressedsize = sizestats[1]
+        meanerror = rundata[3][2]
+        stderror = rundata[3][3]
+        print(rundata)
+        print(meanerror, stderror)
+
+        dpy_sizes[0] += compressedsize
+        dpy_errors[0] += meanerror
+        dpy_stds[0] += stderror
+
+    return dpy_sizes, dpy_errors, dpy_stds
 
   @staticmethod
   def run_qfib(qfib_files, qfib_bits):
@@ -16,7 +142,8 @@ class Sprinter:
             compressedsize = sizestats[1]
             meanerror = rundata[3][2]
             stderror = rundata[3][3]
-            
+            # print(rundata)
+            # print(meanerror, stderror)
             # if meanerror > 2:
             #     continue
             qfib_sizes[i] += compressedsize
@@ -33,7 +160,7 @@ class Sprinter:
     return qfib_sizes, qfib_errors, qfib_stds
 
   @staticmethod
-  def run_trako(config, tko_files, tko_bits):
+  def run_trako(config, tko_files, tko_bits, coords_only=True):
 
       config_ = dict(config)
 
@@ -52,7 +179,7 @@ class Sprinter:
                   config[c]['quantization_bits'] = b # update bits for config
               
               try:
-                  rundata = runner.Runner.tko(f[0], f[1], config=config, coords_only=True, force=False)
+                  rundata = runner.Runner.tko(f[0], f[1], config=config, coords_only=coords_only, force=False)
               except:
                   fails[i] -= 1
                   continue
